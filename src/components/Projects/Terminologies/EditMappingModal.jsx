2ade04dc7957e193b64e47b2b1c8f10d085d4d2e
Yelena Cox <yelenacox@gmail.com>
2024-03-06 15:27:03 -0600

    fixed error with mappings only showing the initial code they are mapped to

--- src/components/Projects/Terminologies/EditMappingModal.jsx
+++ src/components/Projects/Terminologies/EditMappingModal.jsx
@@ -1,7 +1,29 @@
 import { Modal } from 'antd';
 
-export const EditMappingsModal = ({ editMappings, setEditMappings }) => {
-  console.log(editMappings);
+export const EditMappingsModal = ({
+  editMappings,
+  setEditMappings,
+  mapping,
+}) => {
+  const mappingMatch = () => {
+    if (mapping.length > 0) {
+      return mapping.map((m, index) => {
+        if (m?.code === editMappings?.code) {
+          return m?.mappings?.map((item, innerIndex) => (
+            <>
+              <div className="mappings_container">
+                <div key={innerIndex}>Code: {item.code}</div>
+                <div>Display: {item.display}</div>
+              </div>
+            </>
+          ));
+        }
+        return null; // Returning null if condition doesn't match
+      });
+    }
+    return null; // Returning null if mapping length is 0
+  };
+
   return (
     <Modal
       open={!!editMappings}
@@ -13,14 +35,7 @@ export const EditMappingsModal = ({ editMappings, setEditMappings }) => {
       maskClosable={true}
       destroyOnClose={true}
     >
-      {/* {editMappings?.map(item => {
-        return (
-          <>
-            <div>Code: {item.code}</div>
-            <div>Display: {item.display}</div>
-          </>
-        );
-      })} */}
+      {mappingMatch()}
     </Modal>
   );
 };
